2ed336510ec9c788496e6c8c9abc24a08c48f0d5
Praveen19ce <praveen19ce@gmail.com>
2019-09-13 07:18:32 +0530

    added12

--- src/java/A.java
+++ src/java/A.java
@@ -5,6 +5,12 @@ public class A {
 		System.out.println("123");
 		System.out.println("234");
 		System.out.println("welcome");
+		System.out.println("praveen");
+		System.out.println("added");
+		System.out.println("teft");
+		System.out.println("prVEEN");
+		System.out.println("ver");
+
 	}
 
 }
